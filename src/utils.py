f1dab293a1fb865f4f1d6d00c0fb7059715b04bd
Sheiphan <sheiphanshaijan@gmail.com>
2023-03-29 15:46:03 +0530

    'Prediction_Pipeline'

--- src/utils.py
+++ src/utils.py
@@ -4,12 +4,16 @@ import sys
 import numpy as np 
 import pandas as pd
 import dill
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
+from src.logger import logging
+
 
 def save_object(file_path, obj):
+    logging.info("Inside save_object")
     try:
         dir_path = os.path.dirname(file_path)
 
@@ -21,7 +25,9 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
+    
 def evaluate_models(X_train,y_train,X_test,y_test,models):
+    logging.info("Inside evaluate_models")
     try:
         report = {}
         
@@ -41,5 +47,14 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
             
         return report
     
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def load_object(file_path):
+    logging.info("Inside load_object")
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return pickle.load(file_obj)
+        
     except Exception as e:
         raise CustomException(e,sys)
